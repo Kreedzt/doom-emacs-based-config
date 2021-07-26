5f8014d8ba6241a48eeeacfb284caad84517dfe2
Kreedzt <zhaozisong1@live.com>
2021-07-26 09:44:29 +0800

    :sparkles: Kreedzt - add swiper keymap

--- config.el
+++ config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Source Code Pro" :size 28 :weight 'normal)
-       doom-variable-pitch-font (font-spec :family "sans" :size 24))
+(setq doom-font (font-spec :family "Source Code Pro" :size 20 :weight 'normal)
+       doom-variable-pitch-font (font-spec :family "sans" :size 18))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -59,6 +59,9 @@
 
 ;;Key Configuration for Doom as Vanilla Emacs
 (setq evil-default-state 'emacs)
+
+;; 设置 Swiper Keymap
+(global-set-key "\C-s" 'swiper)
 
 ; (set-fontset-font t nil "Symbola" nil 'prepend)
 

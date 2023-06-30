367eda34152d480dc8db5a9c4c705b392599c8ea
Kreedzt <zhaozisong1@live.com>
2023-06-30 16:45:05 +0800

    :sparkles: update font

---
+++ README.md
@@ -0,0 +1,8 @@
+# 基于 DOOM Emacs 的二次配置
+
+## 环境要求
+
+### 字体
+
+- [思源黑体](https://github.com/adobe-fonts/source-han-sans/tree/release)
+- [Source Code Pro](https://fonts.google.com/specimen/Source+Code+Pro)
--- config.el
+++ config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Source Code Variable" :size 16)
-      doom-variable-pitch-font (font-spec :family "Microsoft Sans Serif" :size 12))
+(setq doom-font (font-spec :family "Source Code Pro" :size 16)
+      doom-variable-pitch-font (font-spec :family "思源黑体 VF" :size 12))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the

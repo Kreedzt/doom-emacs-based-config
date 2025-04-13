0fded13ffa911d35db8608aae3830de013d9d36b
Kreedzt <zhaozisong1@live.com>
2025-04-13 09:24:28 +0800

    :wrech: update config

--- README.md
+++ README.md
@@ -6,3 +6,13 @@
 
 - [思源黑体](https://github.com/adobe-fonts/source-han-sans/tree/release)
 - [Source Code Pro](https://fonts.google.com/specimen/Source+Code+Pro)
+
+### lsp-bridge 配置
+
+``` sh
+python3 -m venv ~/global_deps
+source ~/global_deps/bin/activate
+python3 -m pip install epc orjson sexpdata six setuptools paramiko rapidfuzz watchdog
+```
+
+完成后重新执行 `~/.emacs.d/bin/doom sync` 使其 env 生效
--- init.el
+++ init.el
@@ -20,7 +20,7 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       ;;company           ; the ultimate code completion backend
+       company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ivy               ; a search engine for love and life
@@ -92,7 +92,7 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp               ; M-x vscode
+       lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
